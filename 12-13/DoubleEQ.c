0be9d43533e11014140a7ef852ee6417afeefe36
Naresh Rajkumar <nareshrajkumar2002@gmail.com>
2024-03-05 12:51:04 +0530

    4th commit

--- 12-13/DoubleEQ.c
+++ 12-13/DoubleEQ.c
@@ -1,22 +1,42 @@
-// Implementation of Double Ended Queue using static memory allocation.
+// Implementation of Double Ended Queue using dynamic memory allocation.
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MAX 10
+#define INITIAL_CAPACITY 2
 
 typedef struct Queue {
-    int arr[MAX];
+    int *arr;
     int front;
     int rear;
+    int CAP;
 } Que;
 
 void initQue(Que* q) {
     q->front = -1;
     q->rear = -1;
+    q->CAP = INITIAL_CAPACITY;
+    q->arr = (int*)malloc(q->CAP * sizeof(int));
 }
 
 int isEmpty(Que* q) {
     return ((q->front == -1) && (q->rear == -1));
+}
+
+void resize(Que* q) {
+    int newCapacity = q->CAP * 2;
+    int *newArr = (int*)malloc(newCapacity * sizeof(int));
+    
+    // Copy elements to the new array
+    for (int i = q->rear, j = q->rear; i >= q->front; i--, j--) {
+        newArr[j] = q->arr[i];
+    }
+
+    free(q->arr);
+    q->arr = newArr;
+    q->front = 0;
+    q->rear = q->CAP - 1;
+    q->CAP = newCapacity;
+    printf("Queue resized to %d capacity\n", q->CAP);
 }
 
 void display(Que* q) {
@@ -31,15 +51,31 @@ void display(Que* q) {
 }
 
 void insertAtFront(Que* q, int val) {
-    if(isEmpty(q)) {
-        q->front = 0;
-        q->arr[++q->rear] = val;
+    if (isEmpty(q)) {
+        q->front = q->rear = 0;
+        q->arr[q->front] = val;
         printf("Inserted %d at front\n", val);
-    } else if(q->front > 0) {
-        q->arr[--q->front] = val;
+    } else if (q->rear < q->CAP - 1) {
+        // Shift existing elements to make room for new element
+        for (int i = q->rear + 1; i > q->front; i--) {
+            q->arr[i] = q->arr[i - 1];
+        }
+        q->rear++;
+        q->arr[q->front] = val;
         printf("Inserted %d at front\n", val);
-    } else
+    } else {
         printf("Front insertion not possible\n");
+        printf("Resizing...\n");
+        resize(q);
+        // After resizing, update rear index
+        q->rear++;
+        // Shift existing elements to make room for new element
+        for (int i = q->rear; i > q->front; i--) {
+            q->arr[i] = q->arr[i - 1];
+        }
+        q->arr[q->front] = val;
+        printf("Inserted %d at front\n", val);
+    }
 }
 
 void insertAtRear(Que* q, int val) {
@@ -47,11 +83,16 @@ void insertAtRear(Que* q, int val) {
         q->front = 0;
         q->arr[++q->rear] = val;
         printf("Inserted %d at rear\n", val);
-    } else if(q->rear != MAX-1) {
+    } else if(q->rear != q->CAP - 1) {
         q->arr[++q->rear] = val;
         printf("Inserted %d at rear\n", val);
-    } else
+    } else {
         printf("Rear insertion is not possible\n");
+        printf("Resizing...\n");
+        resize(q);
+        q->arr[++q->rear] = val;
+        printf("Inserted %d at rear\n", val);
+    }
 }
 
 int deleteAtFront(Que* q) {
@@ -83,36 +124,54 @@ int deleteAtRear(Que* q) {
 int main() {
     Que q;
     initQue(&q);
-    int choice, val;
+    int choice, data;
     do {
-        printf("\n1- Insert at front\n2- Insert at rear\n");
-        printf("3- Delete at front\n4- Delete at rear\n");
-        printf("5- Display\n6- Exit\n");
+        printf("\nQueue Operations Menu\n");
+        printf("1. Insert at Front\n");
+        printf("2. Insert at Rear\n");
+        printf("3. Delete at Front\n");
+        printf("4. Delete at Rear\n");
+        printf("5. Display\n");
+        printf("6. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch(choice) {
-            case 1: printf("Enter value to insert at front: ");
-                    scanf("%d", &val);
-                    insertAtFront(&q, val);
-                    break;
-            case 2: printf("Enter value to insert at rear: ");
-                    scanf("%d", &val);
-                    insertAtRear(&q, val);
-                    break;
-            case 3: val = deleteAtFront(&q);
-                    if(val != -1)
-                        printf("Deleted element at front is: %d\n", val);
-                    break;
-            case 4: val = deleteAtRear(&q);
-                    if(val != -1)
-                        printf("Deleted element at rear is: %d\n", val);
-                    break;
-            case 5: display(&q);
-                    break;
-            case 6: printf("Exiting...\n");
-                    break;
-            default: printf("Invalid Choice\n... Retry");
+        switch (choice) {
+            case 1:
+                printf("Enter the data to insert at front: ");
+                scanf("%d", &data);
+                insertAtFront(&q, data);
+                display(&q);
+                break;
+            case 2:
+                printf("Enter the data to insert at rear: ");
+                scanf("%d", &data);
+                insertAtRear(&q, data);
+                display(&q);
+                break;
+            case 3:
+                data = deleteAtFront(&q);
+                if(data != -1)
+                    printf("Deleted element from front: %d\n", data);
+                display(&q);
+                break;
+            case 4:
+                data = deleteAtRear(&q);
+                if(data != -1)
+                    printf("Deleted element from rear: %d\n", data);
+                display(&q);
+                break;
+            case 5:
+                display(&q);
+                break;
+            case 6:
+                printf("Exiting program...\n");
+                break;
+            default:
+                printf("Invalid choice! Please enter a valid option.\n");
         }
-    } while(choice != 6);
+    } while (choice != 6);
+    
+    free(q.arr);
+    
     return 0;
 }
